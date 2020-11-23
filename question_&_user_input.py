77d7268edc1d4c26fbe9ad9200e4dd7e5c1f53d3
iamscifi <31117353+iamscifi@users.noreply.github.com>
2020-11-23 11:58:33 -0600

    Update question_&_user_input.py

--- question_&_user_input.py
+++ question_&_user_input.py
@@ -2,4 +2,4 @@ question="Is this a different question?"
 print(question,end=' ')#asks a variable question & places user input on the same line
 cc=input()#takes user input
 
-print(f"\tYou love {cc}")#prints user input with a tab
+print(f"\tYou answered {cc}")#prints user input with a tab
