503b84412f7155686264d826b40d7a2145a2252d
iamscifi <31117353+iamscifi@users.noreply.github.com>
2020-11-23 10:56:14 -0600

    Create question_&_user_input.py

---
+++ question_&_user_input.py
@@ -0,0 +1,5 @@
+question="What's your fave cryptocurrency?"
+print(question,end=' ')#asks a variable question & places user input on the same line
+cc=input()#takes user input
+
+print(f"\tYou love {cc}")#prints user input with a tab
